beb0ee55b66c450780ff64f990a36ac78943919e
Landry Houston <Landry.houston99@gmail.com>
2025-01-24 05:28:29 +0000

    Turtle

---
+++ Day_016/Turtle.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+timmy = Turtle()
+print(timmy)
+timmy.shape('turtle')
+timmy.color('coral')
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+timmy.left(90)
+timmy.forward(200)
+
+my_screen = Screen()
+my_screen.exitonclick()
