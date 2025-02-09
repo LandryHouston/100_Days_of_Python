209f0889f096de9df4d0f5367224ac1d95c00ebd
Landry Houston <landry.houston99@gmail.com>
2025-02-08 23:47:50 -0800

    Completed Day 31

--- Day_031/main.py
+++ Day_031/main.py
@@ -6,28 +6,49 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 FONT_NAME = 'Ariel'
 
-df = pd.read_csv("data/es_en_top1000.csv").to_dict(orient="records")
+current_card = {}
+to_learn = {}
 
+try:
+    data = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/Spanish_English.csv")
+    to_learn = original_data.to_dict(orient='records')
+else:
+    to_learn = data.to_dict(orient="records")
 
 def next_card():
     # Pick a random word and language
-    randchoice = random.choice(df)
-    canvas.itemconfig(card_title, text=list(randchoice.keys())[0])
-    canvas.itemconfig(card_word, text=list(randchoice.values())[0])
-
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text=list(current_card.keys())[0], fill='black')
+    canvas.itemconfig(card_word, text=list(current_card.values())[0], fill='black')
+    canvas.itemconfig(card_background, image=card_front_img)
+    flip_timer = window.after(3000, func=flip_card)
 
 def flip_card():
-    canvas.item
+    canvas.itemconfig(card_title, text=list(current_card.keys())[1], fill='white')
+    canvas.itemconfig(card_word, text=list(current_card.values())[1], fill='white')
+    canvas.itemconfig(card_background, image=card_back_img)
+
+def is_known():
+    to_learn.remove(current_card)
+    data = pd.DataFrame(to_learn)
+    data.to_csv('data/words_to_learn.csv', index=False)
+    next_card()
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-window.after(3000, func=flip_card)
+flip_timer = window.after(3000, func=flip_card)
 
 canvas = Canvas(height=526, width=800)
-logo_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=logo_img)
+card_front_img = PhotoImage(file="images/card_front.png")
+card_back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=card_front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=2)
 card_title = canvas.create_text(400, 150, font=(FONT_NAME, 40, "italic"), tags="word_text")
@@ -38,9 +59,8 @@ unknown_button = Button(image=cross_image, highlightthickness=0, command=next_ca
 unknown_button.grid(column=0, row=1)
 
 check_image = PhotoImage(file="images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=next_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(column=1, row=1)
-
 
 next_card()
 
