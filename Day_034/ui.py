1a505d20c8368a152c919d46240243fd00faf869
Landry Houston <landry.houston99@gmail.com>
2025-02-11 19:20:55 -0800

    Created UI

--- Day_034/ui.py
+++ Day_034/ui.py
@@ -1,2 +1,40 @@
+from tkinter import *
+
 THEME_COLOR = "#375362"
 
+class QuizInterface:
+    def __init__(self, quiz_brain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20, pady=20, bg=THEME_COLOR)
+
+        self.score_label = Label(text="Score: 0", fg="white", bg=THEME_COLOR)
+        self.score_label.grid(row=0, column=1)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width= 280,
+            text="Some Question Text",
+            fill=THEME_COLOR,
+            font=("Ariel", 20, "italic"),
+        )
+        self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
+
+        self.true_image = PhotoImage(file="images/false.png")
+        self.true_button = Button(image=self.true_image, highlightthickness=0, command=None)
+        self.true_button.grid(column=1, row=2)
+
+        self.false_image = PhotoImage(file="images/true.png")
+        self.false_button = Button(image=self.false_image, highlightthickness=0, command=None)
+        self.false_button.grid(column=0, row=2)
+        
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
