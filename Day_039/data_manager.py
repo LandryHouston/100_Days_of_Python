52400dc90d56c2bbf1e9ca19b1b8d5c8f8bd7fef
Landry Houston <landry.houston99@gmail.com>
2025-02-19 18:38:25 -0800

    Starting for scratch

--- Day_039/data_manager.py
+++ Day_039/data_manager.py
@@ -1,31 +1,3 @@
-import yaml
-import requests
-
-with open("config.yaml", "r") as f:
-    config = yaml.safe_load(f)
-
-SHEETY_TOKEN = config['SHEETY_TOKEN']
-sheet_endpoint = "https://api.sheety.co/3336933bf23a9a760f0bd6357f04c82a/flightDealsPython/prices"
-bearer_headers = {"Authorization": f"Bearer {SHEETY_TOKEN}"}
-
-
 class DataManager:
-    # This class is responsible for talking to the Google Sheet.
-    def __init__(self):
-        self.destination_data = {}
-
-    def get_destination_data(self):
-        response = requests.get(url=sheet_endpoint, headers=bearer_headers)
-        data = response.json()
-        self.destination_data = data["prices"]
-        return self.destination_data
-
-    def update_destination_codes(self):
-        for city in self.destination_data:
-            new_data = {"price": {"iataCode": city["iataCode"]}}
-            response = requests.put(
-                url=f"{sheet_endpoint}/{city['id']}",
-                json=new_data,
-                headers=bearer_headers
-            )
-            print(response.text)
+    #This class is responsible for talking to the Google Sheet.
+    pass
--- Day_039/flight_search.py
+++ Day_039/flight_search.py
@@ -1,6 +1,3 @@
 class FlightSearch:
-    # This class is responsible for talking to the Flight Search API.
-
-    def get_destination_code(self, city_name):
-        code = "TESTING"
-        return code
+    #This class is responsible for talking to the Flight Search API.
+    pass
