8170626fae74e1d79692933c2c48dff61c82e1cf
Landry Houston <landry.houston99@gmail.com>
2025-02-20 19:01:32 -0800

    Search for cheaper flights from LAS to other airports

--- Day_039/flight_search.py
+++ Day_039/flight_search.py
@@ -1,5 +1,6 @@
 import yaml
 import requests
+from datetime import datetime
 
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
@@ -8,6 +9,7 @@ TOKEN_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
 AMADEUS_API_KEY = config["AMADEUS_API_KEY"]
 AMADEUS_SECRET = config['AMADEUS_SECRET']
 IATA_ENDPOINT = "https://test.api.amadeus.com/v1/security/oauth2/token"
+FLIGHT_ENDPOINT = "https://test.api.amadeus.com/v2/shopping/flight-offers"
 
 class FlightSearch:
 
@@ -20,7 +22,7 @@ class FlightSearch:
         query = {
             "keyword": city_name,
             "subType": "AIRPORT",
-            "page[limit]": "2",  # Use the correct pagination parameter
+            "page[limit]": "2",
         }
         response = requests.get(
             url="https://test.api.amadeus.com/v1/reference-data/locations",
@@ -52,3 +54,34 @@ class FlightSearch:
         print(f"Your token is {response.json()['access_token']}")
         print(f"Your token expires in {response.json()['expires_in']} seconds")
         return response.json()['access_token']
+
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        headers = {"Authorization": f"Bearer {self._token}"}
+        query = {
+            "originLocationCode": origin_city_code,
+            "destinationLocationCode": destination_city_code,
+            "departureDate": from_time.strftime("%Y-%m-%d"),
+            "returnDate": to_time.strftime("%Y-%m-%d"),
+            "adults": 1,
+            "nonStop": "true",
+            "currencyCode": "USD",
+            "max": "10",
+        }
+
+        response = requests.get(
+            url=FLIGHT_ENDPOINT,
+            headers=headers,
+            params=query,
+        )
+
+        if response.status_code != 200:
+            print(f"check_flights() response code: {response.status_code}")
+            print("There was a problem with the flight search.\n"
+                  "For details on status codes, check the API documentation:\n"
+                  "https://developers.amadeus.com/self-service/category/flights/api-doc/flight-offers-search/api"
+                  "-reference")
+            print("Response body:", response.text)
+            return None
+
+        return response.json()
