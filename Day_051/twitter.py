bc205ff705fe0462cc5fa34cf046da1afe4378bf
Landry Houston <landry.houston99@gmail.com>
2025-03-02 15:26:01 -0800

    Completed Day 51

--- Day_051/twitter.py
+++ Day_051/twitter.py
@@ -14,55 +14,55 @@ PASSWORD = config['PASSWORD']
 PHONE = config['PHONE']
 
 
-class InternetSpeedTwitterBot:
-    def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.up = 0
-        self.down = 0
+options = webdriver.ChromeOptions()
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36 Edg/133.0.0.0")
+options.add_experimental_option("detach", True)
 
-    def get_internet_speed(self):
-        self.driver.get("https://www.speedtest.net/")
-
-        time.sleep(3)
-
-        go_button = self.driver.find_element(By.CSS_SELECTOR, value=".start-button a")
-        go_button.click()
-
-        time.sleep(60)
-        try:
-            self.driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a').click()
-        except:
-            pass
-        self.down = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
-        self.up = self.driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
-        print(self.down, self.up)
-
-    def tweet_at_provider(self):
-        self.driver.get("https://twitter.com/login")
-        time.sleep(3)
-        #time.sleep(2)
-        #email = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[1]/label/div/div[2]/div/input')
-        #password = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div[1]/form/div/div[2]/label/div/div[2]/div/input')
-#
-        #email.send_keys(TWITTER_EMAIL)
-        #password.send_keys(TWITTER_PASSWORD)
-        #time.sleep(2)
-        #password.send_keys(Keys.ENTER)
-#
-        #time.sleep(5)
-        #tweet_compose = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div/div')
-#
-        #tweet = f"Hey Internet Provider, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up?"
-        #tweet_compose.send_keys(tweet)
-        #time.sleep(3)
-#
-        #tweet_button = self.driver.find_element(By.XPATH, value='//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[4]/div/div/div[2]/div[3]')
-        #tweet_button.click()
-#
-        #time.sleep(2)
-        #self.driver.quit()
+driver = webdriver.Chrome(options)
 
 
-bot = InternetSpeedTwitterBot()
-#bot.get_internet_speed()
-bot.tweet_at_provider()
+def get_internet_speed():
+    global ACTUAL_DOWN, ACTUAL_UP
+    driver.get("https://www.speedtest.net/")
+
+    time.sleep(3)
+
+    go_button = driver.find_element(By.CSS_SELECTOR, value=".start-button a")
+    go_button.click()
+
+    time.sleep(60)
+    try:
+        driver.find_element(By.XPATH, '//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[8]/div/div/div[2]/a').click()
+    except:
+        pass
+    ACTUAL_DOWN = driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[1]/div/div[2]/span').text
+    ACTUAL_UP = driver.find_element(By.XPATH, value='//*[@id="container"]/div[1]/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+
+
+def tweet_at_provider():
+    driver.get('https://twitter.com/login')
+
+    time.sleep(2)
+    email = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[4]/label/div/div[2]/div/input')
+    email.click()
+    email.send_keys(EMAIL, Keys.ENTER)
+    time.sleep(1)
+    phone = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div[2]/label/div/div[2]/div/input')
+    phone.send_keys(PHONE, Keys.ENTER)
+    time.sleep(1)
+    password = driver.find_element(By.XPATH, '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input')
+    password.send_keys(PASSWORD, Keys.ENTER)
+    time.sleep(3)
+    tweet = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div/div/div[2]/div/div/div/div')
+    tweet.click()
+    time.sleep(1)
+    tweet.send_keys(f"Hey @CoxComm, why is my internet speed {ACTUAL_DOWN}down/{ACTUAL_UP}up when I pay for {PROMISED_DOWN}down/{PROMISED_UP}up")
+    post = driver.find_element(By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[2]/div[2]/div/div/div/button')
+    post.click()
+
+
+
+get_internet_speed()
+if ACTUAL_DOWN < PROMISED_DOWN and ACTUAL_UP < PROMISED_UP:
+    tweet_at_provider()
+print(ACTUAL_DOWN, ACTUAL_UP)
